f15bb10032220a478b4ce2a0460f6b2a9b3af200
Chris Clarke <chris.m.clarke@live.co.uk>
2018-05-21 10:10:39 +0100

    Create example production config

---
+++ config/config.prod.ts
@@ -0,0 +1,21 @@
+// example production config file
+export const config = {
+    kobotoolbox: {
+        // the server is the full link to the api server (v1)
+        // e.g. https://kc.kobotoolbox.org/api/v1
+        server: "",
+        // a token can be generated from the web insterface at
+        // https://kc.kobotoolbox.org/[username]/api-token
+        token: "",
+        // alternatively you can use username/pass
+        username:"",
+        password:""
+    },
+    pg:{
+        db_name:"",
+        username:"",
+        host:"",
+        password:"",
+        port:null
+    }
+}
